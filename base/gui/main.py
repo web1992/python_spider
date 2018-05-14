fcf94c74e47190d0405c73f1063bc08867006315
web1992 <554933654@qq.com>
2018-05-14 10:12:02 +0800

    git [fix gui]

--- base/gui/main.py
+++ base/gui/main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-
+# pip install -U wxPython
 
 import wx
-
-app == wx.App()
-win = wx.Farme(None)
+#
+app = wx.App()
+#
+win = wx.Frame(None,title="Hello World")
+#
 win.Show()
+#
 app.MainLoop()
+
